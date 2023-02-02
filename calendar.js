9a1d8d81af066d5ec8475ab24397643fafceec63
syeda-sadia-banu <sadia.banu17@gmail.com>
2023-02-02 16:19:48 +0530

    Month Calendar Project

---
+++ calendar.js
@@ -0,0 +1,51 @@
+const monthEl=document.querySelector(".date h1")
+
+const fullDateEl=document.querySelector(".date p");
+
+const daysEL=document.querySelector(".days")
+
+const monthInx=new Date().getMonth();
+//const lastDay=new Date(new Date().getFullYear(),new Date().getMonth(),0)
+
+const lastDay=new Date(new Date().getFullYear(),monthInx+1,0).getDate()
+
+const firstDay=new Date(new Date().getFullYear(),monthInx,1).getDay();
+console.log(lastDay)
+console.log(firstDay)
+
+
+const months=[
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+//console.log(months[monthInx]);
+
+monthEl.innerText=months[monthInx];
+
+fullDateEl.innerText=new Date().toDateString();
+
+let days="";
+for(let i=firstDay;i>0;i--){
+    days+=`<div class="empty"></div>`;
+}
+
+for(let i=1;i<=lastDay;i++){
+    if(i===new Date().getDate()){
+        days+=`<div class="today">${i}</div>`
+    }else{
+        days+=`<div>${i}</div>`
+    }
+    
+
+}
+daysEL.innerHTML=days;
